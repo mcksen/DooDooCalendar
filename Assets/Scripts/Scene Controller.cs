13f5aa89a3ee3bb2b1a74347ee06688a0af6e16d
mcksen <ksenniamcconnell@gmail.com>
2023-08-09 11:33:53 +0100

    Debug.Log removed

--- Assets/Scripts/Scene Controller.cs
+++ Assets/Scripts/Scene Controller.cs
@@ -51,7 +51,7 @@ public class SceneController : ScriptableObject
         {
             scenesToUnload.Add(name);
         }
-        Debug.Log("pop" + Time.frameCount);
+
     }
 
     public void UnloadScenes()
