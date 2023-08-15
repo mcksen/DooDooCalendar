0076d8fed503f31abeee388d8df113f59128e13c
mcksen <ksenniamcconnell@gmail.com>
2023-08-15 16:40:09 +0100

    Tail position works

--- Assets/Scripts/Pop Up.cs
+++ Assets/Scripts/Pop Up.cs
@@ -8,6 +8,7 @@ using System;
 using System.Runtime.InteropServices;
 using UnityEditor.PackageManager.UI;
 using UnityEditor;
+using UnityEngine.AI;
 
 public class PopUp : MonoBehaviour
 {
@@ -15,6 +16,7 @@ public class PopUp : MonoBehaviour
     [SerializeField] ButtonCell buttonCellPrefab;
     [SerializeField] GridLayoutGroup grid;
     [SerializeField] RectTransform locationDependantArea;
+    [SerializeField] RectTransform tail;
 
 
     List<ButtonCell> list = new();
@@ -42,10 +44,11 @@ public class PopUp : MonoBehaviour
 
 
 
-        Vector3[] array = new Vector3[4];
-        locationDependantArea.transform.position = cellPosition;
-        locationDependantArea.GetWorldCorners(array);
 
+        Vector3[] array = new Vector3[4];
+        locationDependantArea.position = cellPosition;
+        locationDependantArea.GetWorldCorners(array);
+        tail.position = new Vector3(locationDependantArea.position.x, tail.position.y, tail.position.z);
         float minXbound = array[0].x;
         float maxXbound = array[3].x;
         float minPadding = 50f;
@@ -54,15 +57,26 @@ public class PopUp : MonoBehaviour
         if (minXbound <= 0)
         {
 
-            locationDependantArea.transform.position = new Vector3(halfPopWidth + minPadding, cellPosition.y, cellPosition.z);
-
+            locationDependantArea.position = new Vector3(halfPopWidth + minPadding, cellPosition.y, cellPosition.z);
+            locationDependantArea.GetWorldCorners(array);
         }
         else if (maxXbound >= Screen.width)
         {
-            locationDependantArea.transform.position = new Vector3(Screen.width - (halfPopWidth + minPadding), cellPosition.y, cellPosition.z);
-
+            locationDependantArea.position = new Vector3(Screen.width - (halfPopWidth + minPadding), cellPosition.y, cellPosition.z);
+            locationDependantArea.GetWorldCorners(array);
         }
-
+        tail.position = new Vector3(cellPosition.x, tail.position.y, tail.position.z);
+        Vector3[] tailArray = new Vector3[4];
+        tail.GetWorldCorners(tailArray);
+        float halfTailWidth = (tailArray[3].x - tailArray[0].x) / 2;
+        if (tailArray[0].x < array[0].x)
+        {
+            tail.position = new Vector3(cellPosition.x + halfTailWidth, tail.position.y, tail.position.z);
+        }
+        else if (tailArray[3].x > array[3].x)
+        {
+            tail.position = new Vector3(cellPosition.x - halfTailWidth, tail.position.y, tail.position.z);
+        }
 
 
     }
