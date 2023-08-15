b84425763a18bed02ac387158e54e7d58044ef67
mcksen <ksenniamcconnell@gmail.com>
2023-08-15 17:21:10 +0100

    buton tint set

--- Assets/Scripts/Pop Up.cs
+++ Assets/Scripts/Pop Up.cs
@@ -40,44 +40,48 @@ public class PopUp : MonoBehaviour
 
     public void SetPosition(Vector3 cellPosition)
     {
-
-
-
-
-
-        Vector3[] array = new Vector3[4];
+        Vector3[] popupArray = new Vector3[4];
         locationDependantArea.position = cellPosition;
-        locationDependantArea.GetWorldCorners(array);
+        locationDependantArea.GetWorldCorners(popupArray);
         tail.position = new Vector3(locationDependantArea.position.x, tail.position.y, tail.position.z);
-        float minXbound = array[0].x;
-        float maxXbound = array[3].x;
-        float minPadding = 50f;
 
+        float minXbound = popupArray[0].x;
+        float maxXbound = popupArray[3].x;
+        float minPadding = 50f;
         float halfPopWidth = (maxXbound - minXbound) / 2;
+
         if (minXbound <= 0)
         {
-
             locationDependantArea.position = new Vector3(halfPopWidth + minPadding, cellPosition.y, cellPosition.z);
-            locationDependantArea.GetWorldCorners(array);
+            locationDependantArea.GetWorldCorners(popupArray);
         }
         else if (maxXbound >= Screen.width)
         {
             locationDependantArea.position = new Vector3(Screen.width - (halfPopWidth + minPadding), cellPosition.y, cellPosition.z);
-            locationDependantArea.GetWorldCorners(array);
+            locationDependantArea.GetWorldCorners(popupArray);
         }
+        SetTailPosition(cellPosition, popupArray);
+
+
+    }
+
+
+
+    private void SetTailPosition(Vector3 cellPosition, Vector3[] containerArray)
+    {
+
         tail.position = new Vector3(cellPosition.x, tail.position.y, tail.position.z);
         Vector3[] tailArray = new Vector3[4];
         tail.GetWorldCorners(tailArray);
         float halfTailWidth = (tailArray[3].x - tailArray[0].x) / 2;
-        if (tailArray[0].x < array[0].x)
+        if (tailArray[0].x < containerArray[0].x)
         {
             tail.position = new Vector3(cellPosition.x + halfTailWidth, tail.position.y, tail.position.z);
         }
-        else if (tailArray[3].x > array[3].x)
+        else if (tailArray[3].x > containerArray[3].x)
         {
             tail.position = new Vector3(cellPosition.x - halfTailWidth, tail.position.y, tail.position.z);
         }
-
 
     }
     public void DestroyObjects()
