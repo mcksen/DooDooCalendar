de5ce131ef325aacf40e15a8cb69998db6ec9b66
mcksen <ksenniamcconnell@gmail.com>
2023-08-15 15:45:32 +0100

    Pop up position is always withing screen boundaries

--- Assets/Scripts/Pop Up.cs
+++ Assets/Scripts/Pop Up.cs
@@ -5,20 +5,26 @@ using TMPro;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
-
+using System.Runtime.InteropServices;
+using UnityEditor.PackageManager.UI;
+using UnityEditor;
 
 public class PopUp : MonoBehaviour
 {
 
     [SerializeField] ButtonCell buttonCellPrefab;
     [SerializeField] GridLayoutGroup grid;
-    [SerializeField] Transform locationDependantArea;
+    [SerializeField] RectTransform locationDependantArea;
+
 
     List<ButtonCell> list = new();
 
+
+
+
+
     public void Configure(List<Tuple<string, System.Action>> buttonList)
     {
-
         foreach (Tuple<string, System.Action> i in buttonList)
         {
             ButtonCell button = Instantiate(buttonCellPrefab, grid.transform);
@@ -32,7 +38,33 @@ public class PopUp : MonoBehaviour
 
     public void SetPosition(Vector3 cellPosition)
     {
-        locationDependantArea.position = cellPosition;
+
+
+
+
+        Vector3[] array = new Vector3[4];
+        locationDependantArea.transform.position = cellPosition;
+        locationDependantArea.GetWorldCorners(array);
+
+        float minXbound = array[0].x;
+        float maxXbound = array[3].x;
+        float minPadding = 50f;
+
+        float halfPopWidth = (maxXbound - minXbound) / 2;
+        if (minXbound <= 0)
+        {
+
+            locationDependantArea.transform.position = new Vector3(halfPopWidth + minPadding, cellPosition.y, cellPosition.z);
+
+        }
+        else if (maxXbound >= Screen.width)
+        {
+            locationDependantArea.transform.position = new Vector3(Screen.width - (halfPopWidth + minPadding), cellPosition.y, cellPosition.z);
+
+        }
+
+
+
     }
     public void DestroyObjects()
     {
